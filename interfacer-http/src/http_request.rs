9a43bae4d30e420f5059c4d761b13ae8809eaa6f
Hexilee <hexileee@gmail.com>
2019-06-17 11:45:14 +0800

    complete Args::new

--- interfacer-http/src/http_request.rs
+++ interfacer-http/src/http_request.rs
@@ -1,18 +1,73 @@
-use interfacer::http::Method;
+use interfacer::http::{Method, Request};
+use proc_macro::{Diagnostic, Level};
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{AttributeArgs, Ident, TraitItemMethod};
+use syn::export::{Debug, ToTokens};
+use syn::{AttributeArgs, Ident, Lit, LitStr, Meta, NestedMeta, TraitItemMethod};
 
-#[derive(Debug)]
 pub struct Args {
-    pub path: Ident,
-    pub content_type: Option<Ident>,
+    pub path: String,
+    pub content_type: Option<Box<dyn ToTokens>>,
     pub send: bool,
 }
 
+impl Default for Args {
+    fn default() -> Self {
+        Args {
+            path: "".into(),
+            content_type: None,
+            send: true,
+        }
+    }
+}
+
 impl Args {
-    fn new(raw_args: AttributeArgs) -> Self {
-        unimplemented!()
+    fn new(mut raw_args: AttributeArgs) -> Self {
+        let mut args: Args = Default::default();
+        args.try_set_path(raw_args.get(0));
+        args.try_set_content_type(raw_args.get(1));
+        args.try_set_send(raw_args.get(2));
+        args
+    }
+
+    pub fn try_set_path(&mut self, attr: Option<&NestedMeta>) {
+        if let Some(path) = attr {
+            if let NestedMeta::Literal(Lit::Str(lit_str)) = path {
+                self.path = lit_str.value()
+            } else {
+                Diagnostic::new(Level::Error, "request path should be string literal").emit()
+            }
+        }
+    }
+
+    pub fn try_set_content_type(&mut self, attr: Option<&NestedMeta>) {
+        if let Some(content_type) = attr {
+            match content_type {
+                NestedMeta::Literal(lit) => {
+                    if let Lit::Str(_) = lit {
+                        self.content_type = Some(Box::new(lit.clone()))
+                    } else {
+                        Diagnostic::new(Level::Error, "content type should be string literal")
+                            .emit()
+                    }
+                }
+                NestedMeta::Meta(meta) => self.content_type = Some(Box::new(meta.name())),
+            }
+        }
+    }
+
+    pub fn try_set_send(&mut self, attr: Option<&NestedMeta>) {
+        if let Some(send) = attr {
+            if let NestedMeta::Meta(Meta::NameValue(kv)) = send {
+                if kv.ident == "send" {
+                    if let Lit::Bool(send) = &kv.lit {
+                        self.send = send.value;
+                        return;
+                    }
+                }
+            }
+        }
+        Diagnostic::new(Level::Error, "send should be 'send=<true | false>'").emit()
     }
 }
 
