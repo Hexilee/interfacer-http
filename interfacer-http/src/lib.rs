83739a3407e3b9bd40e2afd43991ed4b23b402e1
Hexilee <hexileee@gmail.com>
2019-07-24 10:35:25 +0800

    refactor repo structure

--- interfacer-http/src/lib.rs
+++ interfacer-http/src/lib.rs
@@ -1,57 +1,32 @@
-#![feature(proc_macro_diagnostic)]
+pub use http::{
+    header, header::HeaderMap, method, method::Method, request, request::Request, response,
+    response::Response, status, status::StatusCode, uri, uri::Uri, version, version::Version,
+    HttpTryFrom,
+};
 
-extern crate proc_macro;
+use core::future::Future;
 
-use interfacer::http::{HttpTryFrom, Method};
-use proc_macro::{Diagnostic, Level, TokenStream};
-use syn::{parse_macro_input, AttributeArgs, ItemTrait, TraitItemMethod};
-use darling::FromMeta;
+pub use async_service::{AsyncClient, AsyncService};
 
-#[proc_macro_attribute]
-pub fn http_service(_args: TokenStream, input: TokenStream) -> TokenStream {
-    http_service_impl::implement(parse_macro_input!(input as ItemTrait)).into()
+pub trait HttpClient {
+    type Response: Future;
+    type Body;
+    fn request(&self, req: Request<Self::Body>) -> Self::Response;
 }
 
-fn request(method: &str, raw_args: TokenStream, input: TokenStream) -> TokenStream {
-    let method = Method::try_from(method).unwrap_or_else(|err| {
-        Diagnostic::new(
-            Level::Error,
-            format!("{}, fallback to GET", err.to_string()),
-        )
-            .emit();
-        Method::GET
-    });
-
-    let args = http_request_impl::Args::from_list(&parse_macro_input!(raw_args as AttributeArgs))
-        .unwrap_or_else(|err| {
-            Diagnostic::new(
-                Level::Error,
-                format!("parse service method fails: {}", err.to_string()),
-            )
-                .emit();
-            Default::default()
-        });
-
-    http_request_impl::request(
-        method.as_str(),
-        args,
-        parse_macro_input!(input as TraitItemMethod),
-    )
-        .into()
+pub trait HttpService {
+    type Client: HttpClient;
+    fn get_base_url(&self) -> &Uri;
+    fn get_client(&self) -> &Self::Client;
 }
 
-macro_rules! define_request {
-    ($($method:ident),*) => {
-        $(
-            #[proc_macro_attribute]
-            pub fn $method(args: TokenStream, input: TokenStream) -> TokenStream {
-                request(stringify!($method), args, input)
-            }
-        )*
-    };
+pub trait FromContent<const CONTENT_TYPE: &'static str> {
+    fn from_content(data: &[u8]) -> Self;
 }
 
-define_request!(get, post, put, delete, head, options, connect, patch, trace);
+pub trait ToContent<const CONTENT_TYPE: &'static str> {
+    fn to_content(&self) -> &[u8];
+}
 
-mod http_request_impl;
-mod http_service_impl;
+pub mod async_service;
+pub mod content_type;
