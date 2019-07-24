beab36a811e71902988ec7ae13f3e1f3b54d1fa7
Hexilee <hexileee@gmail.com>
2019-07-24 23:59:43 +0800

    constraint HttpClient::Body: futures::io::AsyncRead

--- interfacer-http/interfacer-http-hyper/src/lib.rs
+++ interfacer-http/interfacer-http-hyper/src/lib.rs
@@ -2,16 +2,23 @@
 
 use http::{Request, Response, Uri};
 use hyper::client::HttpConnector;
-use hyper::{Body, Client, Error};
-use interfacer_http_service::{async_trait, HttpClient, HttpService};
+use hyper::{self, Client, Error};
+use interfacer_http_service::{async_trait, AsyncRead, HttpClient, HttpService};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
+// TODO: use generic Connector
 pub struct AsyncClient {
-    inner: hyper::Client<HttpConnector, Body>,
+    inner: hyper::Client<HttpConnector, hyper::Body>,
 }
 
 pub struct AsyncService {
     client: AsyncClient,
     base_uri: Uri,
+}
+
+pub struct Body {
+    inner: hyper::Body,
 }
 
 impl AsyncClient {
@@ -31,12 +38,40 @@ impl AsyncService {
     }
 }
 
+impl Body {
+    pub fn new(body: hyper::Body) -> Self {
+        Self { inner: body }
+    }
+}
+
+impl From<hyper::Body> for Body {
+    fn from(body: hyper::Body) -> Self {
+        Body::new(body)
+    }
+}
+
+impl AsyncRead for Body {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<std::io::Result<usize>> {
+        unimplemented!()
+    }
+}
+
 #[async_trait]
 impl HttpClient for AsyncClient {
     type Err = Error;
     type Body = Body;
-    async fn request(&self, req: Request<Self::Body>) -> Result<Response<Self::Body>, Self::Err> {
-        self.inner.request(req).await
+    async fn request(&self, req: Request<Vec<u8>>) -> Result<Response<Self::Body>, Self::Err> {
+        let (parts, body) = req.into_parts();
+        let (parts, body) = self
+            .inner
+            .request(Request::from_parts(parts, body.into()))
+            .await?
+            .into_parts();
+        Ok(Response::from_parts(parts, body.into()))
     }
 }
 
--- interfacer-http/interfacer-http-service/src/lib.rs
+++ interfacer-http/interfacer-http-service/src/lib.rs
@@ -4,13 +4,18 @@ pub use http::{
     HttpTryFrom,
 };
 
+pub use futures::{
+    AsyncRead, AsyncWrite, AsyncReadExt, AsyncWriteExt
+};
+
 pub use async_trait::async_trait;
 
+// TODO: use T: AsyncRead as type of Request::Body
 #[async_trait]
 pub trait HttpClient {
     type Err;
-    type Body;
-    async fn request(&self, req: Request<Self::Body>) -> Result<Response<Self::Body>, Self::Err>;
+    type Body: AsyncRead;
+    async fn request(&self, req: Request<Vec<u8>>) -> Result<Response<Self::Body>, Self::Err>;
 }
 
 pub trait HttpService {
@@ -19,17 +24,19 @@ pub trait HttpService {
     fn get_client(&self) -> &Self::Client;
 }
 
+// TODO: use T: AsyncRead as type of data
 pub trait FromContent: Sized {
-    const CONTENT_TYPE: &'static str;
+    const CONTENT_TYPE: &'static str = content_type::APPLICATION_JSON;
     const CHARSET: &'static str = content_type::CHARSET_UTF8;
     type Err;
     fn from_content(data: &[u8]) -> Result<Self, Self::Err>;
 }
 
+// TODO: use T: AsyncRead as type of ret
 pub trait IntoContent {
-    const CONTENT_TYPE: &'static str;
+    const CONTENT_TYPE: &'static str = content_type::APPLICATION_JSON;
     const CHARSET: &'static str = content_type::CHARSET_UTF8;
-    fn into_content(&self) -> &[u8];
+    fn into_content(self) -> Vec<u8>;
 }
 
 pub mod content_type;
