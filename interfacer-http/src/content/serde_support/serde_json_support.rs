fa05b751cfc97c814ee51c591d2791b2da92671a
Hexilee <hexileee@gmail.com>
2019-07-26 22:44:33 +0800

    move decode_data and encode_data to src/content/mod.rs

--- interfacer-http/src/content/serde_support/serde_json_support.rs
+++ interfacer-http/src/content/serde_support/serde_json_support.rs
@@ -1,7 +1,6 @@
-use super::super::encode::find_encoder;
+use super::super::encode::{decode_data, encode_data};
 use super::{FromContentFail, ToContentFail};
 use crate::content_types::ENCODING_UTF8;
-use crate::fail::StringError;
 use crate::{FromContent, ToContent};
 use serde::{de::DeserializeOwned, Serialize};
 
@@ -22,35 +21,5 @@ impl<T: DeserializeOwned> FromContent<"application/json"> for T {
             None | Some(ENCODING_UTF8) => Ok(serde_json::from_slice(data)?),
             Some(encode) => Ok(serde_json::from_str(decode_data(data, encode)?.as_str())?),
         }
-    }
-}
-
-#[cfg(not(feature = "encode"))]
-fn encode_data(raw_data: &str, _encode: &str) -> Result<Vec<u8>, StringError> {
-    panic!("encode feature is disable, please enable it");
-}
-
-#[cfg(feature = "encode")]
-fn encode_data(raw_data: &str, encode: &str) -> Result<Vec<u8>, StringError> {
-    match find_encoder(encode) {
-        Some(encoder) => encoder
-            .encode(raw_data, encoding::EncoderTrap::Strict)
-            .map_err(|err| StringError::new(format!("{}", err))),
-        None => Err(StringError::new("unsupported encoding")),
-    }
-}
-
-#[cfg(not(feature = "encode"))]
-fn decode_data(raw_data: &[u8], _encode: &str) -> Result<String, StringError> {
-    panic!("encode feature is disable, please enable it");
-}
-
-#[cfg(feature = "encode")]
-fn decode_data(raw_data: &[u8], encode: &str) -> Result<String, StringError> {
-    match find_encoder(encode) {
-        Some(encoder) => encoder
-            .decode(raw_data, encoding::DecoderTrap::Strict)
-            .map_err(|err| StringError::new(format!("{}", err))),
-        None => Err(StringError::new("unsupported encoding")),
     }
 }
