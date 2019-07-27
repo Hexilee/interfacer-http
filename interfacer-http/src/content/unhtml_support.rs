b0d360e86589e17af3e823e289dba0df5af53efc
Hexilee <hexileee@gmail.com>
2019-07-27 19:43:33 +0800

    conflicting implementation

--- interfacer-http/src/content/unhtml_support.rs
+++ interfacer-http/src/content/unhtml_support.rs
@@ -1,1 +1,25 @@
 
+use super::encode::decode_data;
+use super::fail::FromContentFail;
+use crate::content_type::ContentType;
+use crate::content_types::*;
+use crate::fail::StringError;
+use crate::FromContent;
+use unhtml::FromHtml;
+use serde::de::DeserializeOwned;
+
+impl<T: FromHtml> FromContent for T {
+    type Err = FromContentFail;
+    fn from_content(data: Vec<u8>, content_type: &ContentType) -> Result<Self, Self::Err> {
+        match content_type.base_type() {
+            TEXT_HTML => Ok(Self::from_html(&decode_data(
+                data,
+                content_type.encoding(),
+            )?)?),
+
+            unsupported => {
+                Err(StringError::new(format!("unsupported content type '{}'", unsupported)).into())
+            }
+        }
+    }
+}
