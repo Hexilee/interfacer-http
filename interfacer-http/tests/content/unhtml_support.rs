b6e91dbf5e9f3b14df6976d9448947213b807e1f
Hexilee <hexileee@gmail.com>
2019-07-27 21:53:02 +0800

    pass test content::unhtml_support::normal

--- interfacer-http/tests/content/unhtml_support.rs
+++ interfacer-http/tests/content/unhtml_support.rs
@@ -1,1 +1,24 @@
-use unhtml_derive::{FromHtml};
+use interfacer_http::polyfill::*;
+use interfacer_http::ContentType;
+use unhtml_derive::FromHtml;
+
+#[derive(FromHtml)]
+#[html(selector = "a")]
+struct Link {
+    #[html(attr = "href")]
+    href: String,
+
+    #[html(attr = "inner")]
+    value: String,
+}
+
+#[test]
+fn normal() {
+    let link = Link::from_content(
+        br#"<a href="https://github.com">Github</a>"#[..].to_vec(),
+        &ContentType::new("text/html", None),
+    )
+    .expect("from html fail");
+    assert_eq!("https://github.com", &link.href);
+    assert_eq!("Github", &link.value);
+}
