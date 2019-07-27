15eafc8c62c6f62dd2be2a758c07289f6f9b3dea
Hexilee <hexileee@gmail.com>
2019-07-27 14:20:46 +0800

    add test serde_support

--- interfacer-http/tests/content/mod.rs
+++ interfacer-http/tests/content/mod.rs
@@ -1,2 +1,8 @@
 #[cfg(any(feature = "serde-base", feature = "serde-full"))]
 mod serde_support;
+use serde_derive::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+struct User {
+    name: String,
+    age: i32,
+}
--- interfacer-http/tests/content/serde_support.rs
+++ interfacer-http/tests/content/serde_support.rs
@@ -1,24 +1,23 @@
-#![cfg(feature = "derive")]
-
-use interfacer_http::{ContentType, FromContent, ToContent};
+use super::User;
 use interfacer_http_util::content_types::APPLICATION_JSON;
-use serde_derive::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
-struct User {
-    name: String,
-    age: i32,
+macro_rules! define_test {
+    ($base_type:expr, $encoding:expr) => {
+        let user = User {
+            name: "hexi".to_owned(),
+            age: 18,
+        };
+        let content_type = interfacer_http::ContentType::new($base_type, $encoding);
+        let data = <User as interfacer_http::ToContent>::to_content(&user, &content_type)
+            .expect(&format!("to '{}' fail", $base_type));
+        let mirror = <User as interfacer_http::FromContent>::from_content(data, &content_type)
+            .expect(&format!("from '{}' fail", $base_type));
+        assert_eq!(user, mirror);
+    };
 }
 
 #[cfg(any(feature = "serde-full", feature = "serde-json"))]
 #[test]
 fn json() {
-    let user = User {
-        name: "hexi".to_owned(),
-        age: 18,
-    };
-    let content_type = ContentType::new(APPLICATION_JSON, None);
-    let data = <User as ToContent>::to_content(&user, &content_type).expect("to json fail");
-    let mirror = <User as FromContent>::from_content(data, &content_type).expect("from json fail");
-    assert_eq!(user, mirror);
+    define_test!(APPLICATION_JSON, None);
 }
