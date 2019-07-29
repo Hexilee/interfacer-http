7af409e5d7fd79c8da978de4269f1add5d998b32
Hexilee <hexileee@gmail.com>
2019-07-29 15:11:47 +0800

    add Parameters

--- interfacer-http/interfacer-http-attribute/src/param.rs
+++ interfacer-http/interfacer-http-attribute/src/param.rs
@@ -1,1 +1,20 @@
+use proc_macro::Diagnostic;
+use proc_macro2::Ident;
+use std::collections::HashMap;
+use syn::Expr;
 
+const VAL: &'static str = "val";
+const HEADER: &'static str = "header";
+const BODY: &'static str = "body";
+
+pub struct Parameters {
+    pub values: HashMap<String, Ident>,
+    pub headers: Vec<(Expr, Ident)>,
+    pub body: Option<Ident>,
+}
+
+impl Parameters {
+    pub fn from_attr() -> Result<Self, Diagnostic> {
+        unimplemented!()
+    }
+}
