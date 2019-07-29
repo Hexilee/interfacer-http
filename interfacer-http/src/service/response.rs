5586a3d778b6f6bb844c71a0fb795684a23654d7
Hexilee <hexileee@gmail.com>
2019-07-29 19:49:20 +0800

    add service::Response

---
+++ interfacer-http/src/service/response.rs
@@ -0,0 +1,42 @@
+use crate::cookie;
+use crate::Result;
+use crate::{http, RequestFail};
+use cookie::Cookie;
+use std::ops::Deref;
+
+pub struct Response<T>(http::Response<T>);
+
+impl<T> Deref for Response<T> {
+    type Target = http::Response<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> From<http::Response<T>> for Response<T> {
+    fn from(resp: http::Response<T>) -> Self {
+        Self(resp)
+    }
+}
+
+impl<T> Response<T> {
+    pub fn cookies(&self) -> Result<Vec<Cookie>> {
+        let mut cookies = Vec::new();
+        for cookie in self.headers().get_all(http::header::SET_COOKIE) {
+            cookies.push(Cookie::parse(cookie.to_str()?)?)
+        }
+        Ok(cookies)
+    }
+}
+
+impl From<http::header::ToStrError> for RequestFail {
+    fn from(err: http::header::ToStrError) -> Self {
+        RequestFail::custom(err)
+    }
+}
+
+impl From<cookie::ParseError> for RequestFail {
+    fn from(err: cookie::ParseError) -> Self {
+        RequestFail::custom(err)
+    }
+}
