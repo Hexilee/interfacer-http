fde0e75831dcfa5668a9bdcf4ded79b69785b912
Hexilee <hexileee@gmail.com>
2019-08-05 19:14:05 +0800

    using pub extern in utils

--- interfacer-http/interfacer-http-util/src/lib.rs
+++ interfacer-http/interfacer-http-util/src/lib.rs
@@ -1,15 +1,6 @@
+pub extern crate http;
+pub extern crate url;
+pub extern crate futures;
+pub extern crate cookie;
 pub use async_trait::async_trait;
 pub mod content_types;
-
-macro_rules! reexport {
-    ($module:ident) => {
-        pub mod $module {
-            pub use $module::*;
-        }
-    };
-}
-
-reexport!(http);
-reexport!(url);
-reexport!(futures);
-reexport!(cookie);
