0c7bc28e857c03ab3e27a5b9ca13f4caa2ff80a9
Hexilee <hexileee@gmail.com>
2019-08-08 17:13:38 +0800

    add neccessary manifests for members

---
+++ interfacer-http/publish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd interfacer-http-util && cargo test && cargo publish && cd ..
+cd interfacer-http-attribute && cargo test && cargo publish && cd ..
+cargo test --features "serde-full,encode,unhtml-html,derive" && cargo publish
+cd interfacer-http-hyper && cargo test && cargo publish && cd ..
