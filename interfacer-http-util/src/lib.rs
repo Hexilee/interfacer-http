01a442bd79c09f00b1ccf53b36928fd2c137b19f
Hexilee <hexileee@gmail.com>
2019-08-09 18:20:20 +0800

    remove future dependency of util

--- interfacer-http-util/src/lib.rs
+++ interfacer-http-util/src/lib.rs
@@ -1,7 +1,6 @@
 #![feature(concat_idents, const_str_len, const_str_as_bytes)]
 
 pub extern crate cookie;
-pub extern crate futures;
 pub extern crate http;
 pub extern crate url;
 pub use async_trait::async_trait;
