c76292c7157f4789318fa9155b2ec4cd18f7632a
Hexilee <hexileee@gmail.com>
2019-08-20 16:33:15 +0800

    update publish.sh: replace features `encode` with `encoding` when test interfacer_http

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 cd interfacer-http-util && cargo test && cargo publish; cd ..;
 cd interfacer-http-attribute && cargo test && cargo publish; cd ..;
-cargo test --features "serde-full,encode,unhtml-html,derive" && cargo publish;
+cargo test --features "serde-full,encoding,unhtml-html,derive" && cargo publish;
 cd interfacer-http-hyper && cargo test && cargo publish; cd ..;
