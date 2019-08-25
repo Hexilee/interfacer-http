84a7ed8ea3c12ce27fba6825305da6ceadfdf0a3
Hexilee <hexileee@gmail.com>
2019-08-25 17:32:21 +0800

    add mock module in proc-macro-tests/http_service.rs

--- proc-macro-tests/http_service.rs
+++ proc-macro-tests/http_service.rs
@@ -1,5 +1,4 @@
 #![feature(custom_attribute, param_attrs)]
-#![cfg(all(feature = "derive", feature = "serde-full"))]
 #![allow(unused_attributes)]
 
 use interfacer_http::{
@@ -25,6 +24,105 @@ trait UserService {
         id: u64,
         age: i32,
         #[body] user: &User,
-        #[header(COOKIE)] cookie: &str
+        #[header(COOKIE)] cookie: &str,
     ) -> Result<Response<User>, Self::Error>;
+}
+
+mod mock {
+    use interfacer_http::{
+        async_trait,
+        http::{self, header::CONTENT_TYPE, Request, Response, Version},
+        url::{self, Url},
+        FromContentError, Helper, HttpClient, ToContentError, Unexpected,
+    };
+    use std::future::Future;
+
+    use derive_more::{Display, From};
+
+    #[derive(Display, Debug, From)]
+    pub enum Error {
+        #[display(fmt = "url parse error: {}", _0)]
+        UrlParseError(url::ParseError),
+
+        #[display(fmt = "http error: {}", _0)]
+        HttpError(http::Error),
+
+        #[display(fmt = "to content error: {}", _0)]
+        ToContentError(ToContentError),
+
+        #[display(fmt = "from content error: {}", _0)]
+        FromContentError(FromContentError),
+
+        #[display(fmt = "{}", _0)]
+        Unexpected(Unexpected),
+    }
+
+    impl std::error::Error for Error {}
+
+    pub type Result<T> = std::result::Result<T, Error>;
+
+    pub struct Client<F> {
+        helper: Helper,
+        handler: fn(Request<Vec<u8>>) -> F,
+    }
+
+    impl<F> Client<F>
+    where
+        F: Future<Output = Result<Response<Vec<u8>>>> + Send + 'static,
+    {
+        pub fn new(base_url: Url, handler: fn(Request<Vec<u8>>) -> F) -> Self {
+            Self {
+                handler,
+                helper: Helper::new().with_base_url(base_url),
+            }
+        }
+    }
+
+    #[async_trait]
+    impl<F> HttpClient for Client<F>
+    where
+        F: Future<Output = Result<Response<Vec<u8>>>> + Send + 'static,
+    {
+        type Err = Error;
+        async fn request(&self, req: Request<Vec<u8>>) -> Result<Response<Vec<u8>>> {
+            (self.handler)(req).await
+        }
+
+        fn helper(&self) -> &Helper {
+            &self.helper
+        }
+    }
+
+    //    async fn login_page_handler(req: Request<Vec<u8>>) -> Result<Response<Vec<u8>>> {
+    //        assert_eq!(
+    //            Url::parse(BASE_URL)?.join("default2.aspx")?.as_str(),
+    //            req.uri()
+    //        );
+    //        assert_eq!("GET", req.method());
+    //        Ok(Response::builder()
+    //            .status(200)
+    //            .version(Version::HTTP_11)
+    //            .header(CONTENT_TYPE, "text/html; charset=gb2312")
+    //            .body(
+    //                encoding_from_whatwg_label("gb2312")
+    //                    .unwrap()
+    //                    .encode(LOGIN_PAGE, EncoderTrap::Strict)
+    //                    .unwrap(),
+    //            )?)
+    //    }
+    //
+    //    #[tokio::test]
+    //    async fn test_login_page() -> Result<()> {
+    //        let service = Client::new(BASE_URL.parse()?, login_page_handler);
+    //        let page = service.get_login_page().await?;
+    //        assert_eq!(
+    //            &page.body().hidden_form,
+    //            &HiddenForm {
+    //                event_argument: "".into(),
+    //                event_target: "".into(),
+    //                view_state: "dDwxNTc0MzA5MTU4Ozs+b5wKASjiu+fSjITNzcKuKXEUyXg=".into(),
+    //            }
+    //        );
+    //        Ok(())
+    //    }
 }
