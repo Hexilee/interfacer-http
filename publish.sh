dc764648fdb0ab876671bb65c9aa96539a708881
Hexilee <hexileee@gmail.com>
2019-08-28 21:01:08 +0800

    remove interfacer-http-util in publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd interfacer-http-util && cargo test && cargo publish; cd ..;
 cd interfacer-http-attribute && cargo test && cargo publish; cd ..;
 cargo test --features "serde-full,encoding,unhtml-html,derive" && cargo publish;
 cd interfacer-http-hyper && cargo test && cargo publish; cd ..;
