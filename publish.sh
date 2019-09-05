ca63a5e3036d2d91ae63b3fb9a1c6080315ffbae
Hexilee <hexileee@gmail.com>
2019-09-05 19:27:41 +0800

    hyper-0.13.0.alpha.1 released, migrate to it

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cargo test --all --features "serde-full,unhtml-html,encoding,derive,runtime,mock"
-cd interfacer-http-attribute && cargo publish; cd ..;
-cargo publish;
-cd interfacer-http-hyper && cargo publish; cd ..;
