cff7af6af1026c6512aee940149df9701873a1a6
Hexilee <hexileee@gmail.com>
2019-09-29 12:29:30 +0800

    complete parse tests

--- interfacer-http-attribute/src/parse.rs
+++ interfacer-http-attribute/src/parse.rs
@@ -5,7 +5,7 @@ use std::convert::TryFrom;
 use syn::parse::{Parse, Parser};
 use syn::{punctuated::Punctuated, Attribute, Meta, NestedMeta, Path, Token};
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub enum AttrMeta {
     Name(Ident),
     List {
@@ -64,5 +64,60 @@ impl AttrMeta {
         } else {
             Ok(path.segments.first().unwrap().ident.clone())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryInto;
+    use syn::ItemFn;
+
+    #[test]
+    fn try_parse_test() -> Result<(), Diagnostic> {
+        let func: ItemFn = try_parse(quote!(
+            fn foo() {}
+        ))?;
+        assert_eq!("foo", &func.sig.ident.to_string());
+        assert!(try_parse::<ItemFn>(quote!(let a = 1;))
+            .unwrap_err()
+            .message()
+            .contains(&format!("parse token({}) fail", quote!(let a = 1;))));
+        Ok(())
+    }
+
+    fn parse_attribute(token: TokenStream) -> Result<Attribute, Diagnostic> {
+        let func: ItemFn = try_parse(quote!(
+            #token
+            fn foo(){}
+        ))?;
+        func.attrs
+            .into_iter()
+            .next()
+            .ok_or(Diagnostic::new(Level::Error, "no recognized attributes"))
+    }
+
+    #[test]
+    fn attr_meta_try_from() -> Result<(), Diagnostic> {
+        let meta: AttrMeta = parse_attribute(quote!(#[foo]))?.try_into()?;
+        assert_eq!("foo", meta.name().to_string());
+        assert!(AttrMeta::try_from(parse_attribute(quote!(#[foo{}]))?)
+            .unwrap_err()
+            .message()
+            .contains(&format!("attr ({}) is not a meta", quote!(#[foo{}]))));
+        assert_eq!(
+            format!("path({}) is not a ident", quote!(foo::bar)),
+            AttrMeta::try_from(parse_attribute(quote!(#[foo::bar]))?)
+                .unwrap_err()
+                .message()
+        );
+        assert_eq!(
+            format!("attr ({}) is not a valid AttrMeta", quote!(#[foo = 1])),
+            AttrMeta::try_from(parse_attribute(quote!(#[foo = 1]))?)
+                .unwrap_err()
+                .message()
+        );
+
+        Ok(())
     }
 }
